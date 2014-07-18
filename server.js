6def0b69e9fc4245a1d888195ef20b8f03d26439
Ben Brewer <bbrewer97202@yahoo.com>
2014-07-18 15:40:16 -0700

    config file implemented

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+//configuration not stored in source control
+var config = require('./config.json');
+
 //configure middleware
 app.use(bodyParser());
 
@@ -12,7 +15,7 @@ var port = process.env.PORT || 8080;
 
 //database
 //todo: connection failure
-
+mongoose.connect(config.mongo);
 var Mountain = require('./app/models/mountain');
 
 //router
